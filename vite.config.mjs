693701274e5ac52888fe2709e6b9a1778360ef68
Ben <Ben.Xian@infocast.com.hk>
2025-01-15 09:35:32 +0800

    update

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,7 @@ import tailwind from 'tailwindcss'
 import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   server:{
     port:5173
   },
